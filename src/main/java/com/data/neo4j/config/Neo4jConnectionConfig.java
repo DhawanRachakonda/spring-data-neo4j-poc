753eb3e746e8812e78d4aeaada420780e53a4e16
Dhawan Rachakonda <r.dhawan27@hotmail.co.uk>
2020-07-11 23:36:55 +0530

    feat: config

---
+++ src/main/java/com/data/neo4j/config/Neo4jConnectionConfig.java
@@ -0,0 +1,32 @@
+package com.data.neo4j.config;
+
+import org.neo4j.ogm.config.ClasspathConfigurationSource;
+import org.neo4j.ogm.config.ConfigurationSource;
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableNeo4jRepositories(basePackages = {"com.data.neo4j.repositories"})
+@EnableTransactionManagement
+public class Neo4jConnectionConfig {
+
+    public SessionFactory sessionFactory() {
+        return new SessionFactory(configuration());
+    }
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration configuration(){
+        ConfigurationSource properties = new ClasspathConfigurationSource("ogm.properties");
+        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration.Builder(properties).build();
+        return configuration;
+    }
+
+    @Bean
+    public Neo4jTransactionManager transactionManager() {
+        return new Neo4jTransactionManager(sessionFactory());
+    }
+}
