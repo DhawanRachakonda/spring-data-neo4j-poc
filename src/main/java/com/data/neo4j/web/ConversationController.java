e8282cac93f571193a0a320b22d653a9a1817694
Dhawan Rachakonda <r.dhawan27@hotmail.co.uk>
2020-07-12 02:33:21 +0530

    feat: endpoint for conversation

---
+++ src/main/java/com/data/neo4j/web/ConversationController.java
@@ -0,0 +1,30 @@
+package com.data.neo4j.web;
+
+import com.data.neo4j.ConversationService;
+import com.data.neo4j.dto.PostConversationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ConversationController {
+
+    private final ConversationService conversationService;
+
+    @Autowired
+    public ConversationController(final ConversationService conversationService) {
+        this.conversationService = conversationService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> saveConversation(@RequestBody PostConversationDTO conversation) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED).
+                body(this.conversationService.createConversation(conversation));
+    }
+}
