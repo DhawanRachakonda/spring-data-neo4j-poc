eb100f986182da8994dedaf57030125aa645a6de
Dhawan Rachakonda <r.dhawan27@hotmail.co.uk>
2020-07-12 01:52:53 +0530

    fix: message and attachments

--- src/main/java/com/data/neo4j/dto/PostConversationDTO.java
+++ src/main/java/com/data/neo4j/dto/PostConversationDTO.java
@@ -2,8 +2,12 @@ package com.data.neo4j.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PostConversationDTO {
 
     private String ticketId;
+    private String message;
+    private List<String> attachments;
 }
